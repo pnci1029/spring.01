9cad4f6ee324756e7c73fdefb55b19d6293ed94b
Ch Hong <pnci1029@gmail.com>
2022-05-24 11:20:05 +0900

    05.24.01

--- src/main/java/HCHH/Spring/repository/ContentRepository.java
+++
@@ -1,7 +0,0 @@
-package HCHH.Spring.repository;
-
-import HCHH.Spring.Entity.EntityContent;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContentRepository extends CrudRepository<EntityContent, Long> {
-}
